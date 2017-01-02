f41be7f243f549922561b4395f33b4e17fc9aaba
Tim Lytle <tim@timlytle.net>
2017-01-02 14:50:15 -0500

    Added license info.

--- src/Resource/Signal.php
+++ src/Resource/Signal.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @copyright Copyright (c) 2017. Timothy Lytle (tjlytle)
+ * @license https://github.com/tjlytle/qclient/LICENSE.txt MIT License
+ */
+
 namespace QClient\Resource;
 
 /**
